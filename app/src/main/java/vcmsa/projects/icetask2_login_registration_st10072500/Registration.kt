1a802ea92a96d0b99379c0506b2409da486de163
PriyaST10072500 <ST10072500@VCCONNECT.EDU.ZA>
2025-03-04 22:34:19 +0200

    Initial commit

---
+++ app/src/main/java/vcmsa/projects/icetask2_login_registration_st10072500/Registration.kt
@@ -0,0 +1,82 @@
+package vcmsa.projects.icetask2_login_registration_st10072500
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class Registration : AppCompatActivity() {
+
+    lateinit var etEmail: EditText
+    lateinit var etConfPass: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnRegistered: Button
+    lateinit var tvRedirectLogin: Button
+
+
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registration)
+
+        etEmail = findViewById(R.id.etSEmailAddress)
+        etConfPass = findViewById(R.id.etSConfPassword)
+        etPass = findViewById(R.id.etSPassword)
+        btnRegistered = findViewById(R.id.btnRegistered)
+        tvRedirectLogin = findViewById(R.id.tvRedirectLogin)
+
+
+        auth = FirebaseAuth.getInstance()
+
+        btnRegistered.setOnClickListener {
+            signUpUser()
+        }
+
+
+        tvRedirectLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun signUpUser() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+        val confirmPassword = etConfPass.text.toString()
+
+
+        if (email.isBlank() || pass.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (pass != confirmPassword) {
+            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
